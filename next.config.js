a06f29b1e30aa15a4bef5d4cc74de2709b56037f
Administrator <anu+admin@1moby.com>
2023-01-10 23:34:45 +1100

    use github-action instead

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = '/route53-to-cloudflare.github.io'
+let basePath = '/route53-to-cloudflare.github.io'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+  
   reactStrictMode: true,
-  assetPrefix: '/route53-to-cloudflare.github.io/out',
-  basePath: '/route53-to-cloudflare.github.io/out'
+  assetPrefix: assetPrefix,
+  basePath: basePath
 }
 
 module.exports = nextConfig
