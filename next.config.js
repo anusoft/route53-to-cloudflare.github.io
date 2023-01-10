9eff0b87a2fd54aa875cdc7a5c66e77610b891be
Administrator <anu+admin@1moby.com>
2023-01-10 23:16:47 +1100

    add basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/route53-to-cloudflare.github.io/out'
+  assetPrefix: '/route53-to-cloudflare.github.io/out',
+  basePath: '/route53-to-cloudflare.github.io/out'
 }
 
 module.exports = nextConfig
